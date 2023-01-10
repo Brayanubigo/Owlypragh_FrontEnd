cce3ed0ea838d007ea5775cd386154e793a20d80
brayan <brayan.ubigo@gmail.com>
2023-01-10 15:40:42 -0300

    agregado un Link

--- src/Component/ContentPorta.jsx
+++ src/Component/ContentPorta.jsx
@@ -50,9 +50,12 @@ const ContentPorta = () => {
     </Link>
  
     <div className='flex items-center absolute mt-0 py-0 my-0 '>
+    <Link to="/GPAISAJES">
     <h1 className='text-center mt-[20rem] text-black text-4xl border-2 border-black hover:bg-white  transform 
      transition duration-500 hover:scale-110  border-solid w-[15rem] py-3 font-mono'>Paisajes</h1>
+    </Link>
     </div>
+    
     </div>
   </div>
 </div>
